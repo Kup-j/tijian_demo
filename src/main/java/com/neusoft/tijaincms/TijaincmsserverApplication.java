1e006a0c75337120939fd23cb22f8efbcbc99fa8
Kup-j <90045403+Kup-j@users.noreply.github.com>
2023-06-30 15:25:56 +0800

    Feature (#3)

---
+++ src/main/java/com/neusoft/tijaincms/TijaincmsserverApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.tijaincms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TijaincmsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TijaincmsserverApplication.class, args);
+	}
+
+}
